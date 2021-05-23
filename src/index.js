112b262e51341079cd2172cf52a3288be72192c7
Stanislav Kruhliak <stanislav.kruhliak@gmail.com>
2021-05-23 17:54:11 +0200

    add parcel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './sass/main.scss';
+import 'material-design-icons';
+import { fetchItems } from './js/apiService';
+import moviesPicturesListTpl from './templates/pictures-list.hbs';
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  input: document.querySelector('input[name=query]'),
+  listPictures: document.querySelector('.gallery'),
+  button: document.querySelector('.button'),
+};
+
+refs.form.addEventListener('submit', onForm);
+
+function onForm(e) {
+  e.preventDefault();
+  // console.log(refs.input.value);
+  getItems(refs.input.value);
+}
+
+async function getItems(items) {
+  try {
+    const result = await fetchItems(items);
+    console.log(result);
+    methodName(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+function methodName(a) {
+  refs.listPictures.textContent = '';
+  const markupItems = moviesPicturesListTpl(a);
+  refs.listPictures.insertAdjacentHTML('beforeend', markupItems);
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,1 +1,11 @@
-'21737964-f95ae509173e22fa890b9705b';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
+
+export const fetchItems = async (searchQuery, page = 1) => {
+  const response = await axios.get(
+    `&q=${searchQuery}&page=${page}&per_page=12&key=21737964-f95ae509173e22fa890b9705b`,
+  );
+  // console.log(response.data.hits);
+  return response.data.hits;
+};
